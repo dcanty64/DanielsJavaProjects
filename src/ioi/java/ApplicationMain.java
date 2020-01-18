9133131f1e6bc76e4ca5b5e74fffe66ade8868b8
DanielCanty <DanielCanty@DanielandGreg>
2020-01-18 15:48:33 +0000

    v1

---
+++ src/ioi/java/ApplicationMain.java
@@ -0,0 +1,39 @@
+/*
+ To change this license header, choose License Headers in Project Properties.
+ To change this template file, choose Tools | Templates
+ and open the template in the editor.
+ */
+package ioi.java;
+
+/**
+
+ @author Conchraidh
+ */
+public class ApplicationMain
+{
+
+	/**
+	 @param args the command line arguments
+	 */
+	public static void main(String[] args)
+	{
+            String name="Daniel";
+            int yearOfBirth=2007;
+            int age=12;
+            int number1=10;
+            int number2=5;
+            int answer=number1*number2;
+            
+            System.out.println(name);
+            System.out.println(age);
+            System.out.println(answer);
+            System.out.println("My name is"+ name +
+                    ".My age is " + age +
+                    ".My answer is " + answer);
+            String outText=("My name is"+ name +
+                    ".My age is " + age +
+                    ".My answer is " + answer)
+          	// TODO code application logic here
+	}
+
+}
